74efcb214dc5e257e8d172451cf9856f5e0b1f00
desaianiruddh <aniruddh.d@simformsolutions.com>
2022-08-10 16:57:58 +0530

    fix bug

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGO_URL:
+    'mongodb+srv://desaianiruddh:anii123@cluster0.b8zam.mongodb.net/socialM?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key',
+};
